73c9ce051c1aab0d055694c30a2a8cdabfbd4771
Truman Wang <tzwang@Trumans-MacBook-Pro.local>
2018-04-27 10:57:29 -0400

    adding tests for Checklogic.py

--- chess/tests/UnitTests.py
+++ chess/tests/UnitTests.py
@@ -16,3 +16,16 @@ assert b_king.player == -1
 print("Passed tests for ChessPieces.py")
 
 print("Running unit tests for ChessGame.py")
+board = logic.Board(8)
+
+assert len(board.pieces["w_pieces"]) == 16
+assert len(board.pieces["b_pieces"]) == 16
+assert len(board.board) ==  8
+assert isinstance(board.board[4,0], piece.King) == True
+assert isinstance(board.board[4,7], piece.King) == True
+assert isinstance(board.board[1,0], piece.Knight) == True
+assert isinstance(board.board[0,0], piece.Rook) == True
+assert board.board[5,5] == None
+
+print("\tRunning unit tests for check_legal type functions")
+
