490be129fc29a00cfc9a47419732c645a9d2ed90
tzwang1 <truman.wang1234@gmail.com>
2018-05-06 18:11:56 -0400

    adding Random move player

---
+++ chess/ChessPlayers.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class RandomPlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        legal_moves = self.game.board.get_legal_actions(self.game.cur_player)
+        num_pieces = len(legal_moves)
+
+        rand_piece = np.random.randint(num_pieces)
+        num_moves = len(legal_moves[rand_piece][1])
+
+        rand_move = np.random.randint(num_moves)
+
+        move = (legal_moves[rand_piece][0], legal_moves[rand_piece][1][rand_move])
+
+        return move
+
