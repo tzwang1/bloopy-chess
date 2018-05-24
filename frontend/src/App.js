9e7f7f14ccd384b82f1dee9c7158704b78bf80df
tzwang1 <truman.wang1234@gmail.com>
2018-05-24 17:06:55 -0400

    Finishing nav bar, and adding logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation'
+import Navigation from './components/Navigation/Navigation';
+import Board from './components/Board/Board'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Board />
+        <div className="f5">Icons made by <a href="https://www.flaticon.com/authors/ddara" title="dDara">dDara</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
       </div>
     );
   }
--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Navigation.css';
+import Logo from './Logo/Logo'
 
 const Navigation = () => {
     return(
-        <nav style={{display: 'flex', justifyContent: 'center'}}>
+        <nav className='nav_bar'>
+            <Logo />
             <p className='f3 link dim black underline pa3 pointer'>Play </p>
             <p className='f3 link dim black underline pa3 pointer'> Stats </p>
             <p className='f3 link dim black underline pa3 pointer'> Sign Up </p>
