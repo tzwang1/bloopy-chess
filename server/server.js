7209e1620ea3b3e9835f002829f937da6be8cfb8
tzwang1 <truman.wang1234@gmail.com>
2018-05-23 22:43:28 -0400

    modularizing code

--- server/server.js
+++ server/server.js
@@ -3,29 +3,7 @@ const amqp = require('amqplib/callback_api');
 
 const app = express();
 
-var args = process.argv.slice(2);
-
-amqp.connect('amqp://localhost', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    ch.assertQueue('', {exclusive: true}, function(err, q) {
-      var corr = generateUuid();
-      var game_type = "two random bots"
-
-      //console.log(' [x] Requesting test(%d)', num);
-
-      ch.consume(q.queue, function(msg) {
-        if (msg.properties.correlationId == corr) {
-          console.log(' [.] Got\n %s', msg.content.toString());
-          setTimeout(function() { conn.close(); process.exit(0) }, 500);
-        }
-      }, {noAck: true});
-
-      ch.sendToQueue('rpc_queue',
-      new Buffer.from(game_type),
-      { correlationId: corr, replyTo: q.queue });
-    });
-  });
-});
+AMQP_HOST = "amqp://localhost"
 
 function generateUuid() {
   return Math.random().toString() +
@@ -33,9 +11,32 @@ function generateUuid() {
          Math.random().toString();
 }
 
-// app.listen(process.env.PORT || 3000, () => {
-//     console.log("app is running on port 3000");
-// });
+app.listen(process.env.PORT || 3000, () => {
+    console.log("app is running on port 3000");
+});
+
+app.get('/twoRandomBots', function(req, res){
+    amqp.connect(AMQP_HOST, function(err, conn) {
+        conn.createChannel(function(err, ch) {
+            ch.assertQueue('', {exclusive: true}, function(err, q) {
+            var corr = generateUuid();
+            var game_type = "two random bots";
+
+            ch.consume(q.queue, function(msg) {
+                if (msg.properties.correlationId == corr) {
+                    console.log(' [.] Got\n %s', msg.content.toString());
+                    res.send(msg.content.toString());
+                    // setTimeout(function() { conn.close(); process.exit(0) }, 500);
+                }
+            }, {noAck: true});
+
+            ch.sendToQueue('rpc_queue',
+            new Buffer.from(game_type),
+            { correlationId: corr, replyTo: q.queue });
+            });
+        });
+    });
+})
 
 
 
