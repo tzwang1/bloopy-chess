2bd14ea1446271b933fc56379815647463834881
tzwang1 <truman.wang1234@gmail.com>
2018-06-08 21:06:50 -0400

    cleaning up

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -6,10 +6,10 @@ import Play from './Play/Play';
 const Navigation = ({ onPlayClick, onStatsClick, onSigninClick }) => {
     return(
         <nav className='nav_bar'>
-            <Logo />
             <Play onPlayClick={onPlayClick}/>
-            <p onClick={onStatsClick} className='f3 link dim black underline pb0 mb0 mr5 mt4 pointer'> Stats </p>
-            <p onClick={onSigninClick} className='f3 link dim black underline pb0 mb0 mr5 mt4 pointer'> Sign Up </p>
+            <p onClick={onStatsClick} className='f3 link dim black underline pb0 mb0 mr3 ml3 mt4 pointer'> Stats </p>
+            <p onClick={onSigninClick} className='f3 link dim black underline pb0 mb0 mr3 ml3 mt4 pointer'> Sign Up </p>
+            <Logo />
         </nav>
     );
 }
