fff485d737e6aab5943c0c3cc76c2ff9e1a3ec53
tzwang1 <truman.wang1234@gmail.com>
2018-06-08 21:05:37 -0400

    adding handleMove prop

--- frontend/src/components/Board/Board.js
+++ frontend/src/components/Board/Board.js
@@ -3,22 +3,22 @@ import './Board.css';
 import DarkTile from './Tile/DarkTile';
 import LightTile from './Tile/LightTile';
 
-const Board = ({ board }) => {
-    console.log(board);
+const Board = (props) => {
+    let board = props.board;
     let board_component = [];
     for(let i = 0; i < 8; i++) {
         for(let j = 0; j < 8; j++) {
             if(i % 2 === 0) {
                 if(j % 2 === 0) {
-                    board_component.push(<DarkTile key={[i,j]} piece={board[i][j]} pos={[i,j]}/>);
+                    board_component.push(<DarkTile key={[i,j]} piece={board[i][j]} pos={[i,j]} handleMove={props.handleMove}/>);
                 } else {
-                    board_component.push(<LightTile key={[i,j]} piece={board[i][j]} pos={[i,j]}/>);
+                    board_component.push(<LightTile key={[i,j]} piece={board[i][j]} pos={[i,j]} handleMove={props.handleMove}/>);
                 }
             } else {
                 if(j % 2 === 0){
-                    board_component.push(<LightTile key={[i,j]} piece={board[i][j]} pos={[i,j]}/>);
+                    board_component.push(<LightTile key={[i,j]} piece={board[i][j]} pos={[i,j]} handleMove={props.handleMove}/>);
                 } else {
-                    board_component.push(<DarkTile key={[i,j]} piece={board[i][j]} pos={[i,j]}/>);
+                    board_component.push(<DarkTile key={[i,j]} piece={board[i][j]} pos={[i,j]} handleMove={props.handleMove}/>);
                 }
             }
         }
