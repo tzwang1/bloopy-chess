fc48db6e0fb72d26135a44524091471825e07802
tzwang1 <truman.wang1234@gmail.com>
2018-06-23 22:57:07 -0400

    adding training function

--- backend/server/model/NNet.py
+++ backend/server/model/NNet.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 
 import torch
 import torch.nn as nn
@@ -31,3 +32,40 @@ class NNet():
         optimizer = optim.Adam(self.nnet.parameters())
 
         for epoch in range(args.epochs):
+            print('EPOCH ::: ' + str(epoch+1))
+            self.nnet.train() # Switch nnet into training mode
+
+            batch_idx = 0
+            while batch_idx < int(len(examples) / args.batch_size):
+                sample_ids = np.random.randint(len(examples), size=args.batch_size)
+                boards, pis, vs = list(zip(*examples[i] for i in sample_ids))
+                boards = torch.FloatTensor(np.array(boards).astype(np.float64))
+                target_pis = torch.FloatTensor(np.array(pis))
+                target_vs = torch.FloatTensor(np.array(vs).astype(np.float64))
+
+                # predict
+                if args.cuda:
+                    boards, target_pis, target_vs = boards.contiguous().cuda(), target_pis.contiguous().cuda(), target_vs.contiguous().cuda()
+                boards, target_pis, target_vs = Variable(boards), Variable(target_pis), Variable(target_vs)
+
+                out_pi, out_v = self.nnet(boards)
+                l_pi = self.loss_pi(target_pis, out_pi)
+                l_v = self.loss_v(target_vs, out_v)
+                total_loss = l_pi + l_v # Note the original paper uses a different loss function (may want to try that, if this one doesn't work well)
+                
+                # compute gradient and do SGD step
+                optimizer.zero_grad()
+                total_loss.backward()
+                optimizer.step()
+
+                batch_idx+=1
+
+                # Plot graph of training data/progress
+
+def loss_pi(self, targets, outputs)
+    return -torch.sum(targets * outputs)/targets.size()[0]
+
+def loss_v(self, targets, outputs)
+    return torch.sum((targets - outputs.view(-1))**2)/targets.size()[0]
+
+
